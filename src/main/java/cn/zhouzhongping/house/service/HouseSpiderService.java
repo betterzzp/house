481cc5e38bcf1ad2638fe5eff3611cea0243a107
zhouzhongping <zhouzhongping@yntengyun.com>
2020-12-10 09:53:04 +0800

    功能完善

--- src/main/java/cn/zhouzhongping/house/service/HouseSpiderService.java
+++ src/main/java/cn/zhouzhongping/house/service/HouseSpiderService.java
@@ -1,5 +1,11 @@
 package cn.zhouzhongping.house.service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.springframework.stereotype.Service;
@@ -16,13 +22,22 @@ public class HouseSpiderService {
 				HttpClient httpClient = new HttpClient();
 				String spcryUrl = url + "/p" + i + "/#filtersort";
 				// 创建一个get（）方法 类似于在浏览器输入地址
-				GetMethod getMethod = new GetMethod(spcryUrl);
-				httpClient.executeMethod(getMethod);
-				// 查看命中情况
-				String htmlContent = getMethod.getResponseBodyAsString();
-				System.out.println(htmlContent);
-				// 释放链接
-				getMethod.releaseConnection();
+				URL url = new URL(spcryUrl);
+				HttpsURLConnection conn = (HttpsURLConnection)url.openConnection();
+				
+				conn.setRequestMethod("GET");
+				conn.setRequestProperty("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36");
+				
+				BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"));
+				
+				String msg = null;
+				
+				StringBuffer sb = new StringBuffer();
+				
+				while(null != (msg = br.readLine())) {
+					sb.append(msg);
+				}
+				System.out.println(sb);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
